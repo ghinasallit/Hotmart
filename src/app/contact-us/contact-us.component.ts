33772569e99d3ae33d1f7571271de1e4f7d5ae75
ghina <ghinasallit@gmail.com>
2022-09-18 21:55:55 +0200

    dashboard

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -7,14 +7,21 @@ import { FormControl, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+  public contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required)
+  });
 
   constructor() {
   }
 
-
+  public get f() {
+    return this.contactForm.controls;
+  }
 
   public submit(){
-    // console.log(this.contactForm.value);
+    console.log(this.contactForm.value);
   }
 
   ngOnInit(): void {
