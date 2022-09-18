f6f59c20d2830deaaaae0d29a6b6dc09b4997bd2
ghina <ghinasallit@gmail.com>
2022-09-18 21:40:17 +0200

    dashboard

---
+++ src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import { FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.scss']
+})
+export class ContactUsComponent implements OnInit {
+
+  constructor() {
+  }
+
+
+
+  public submit(){
+    // console.log(this.contactForm.value);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
